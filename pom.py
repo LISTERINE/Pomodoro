445ee74fb61758cc767780410ae651e71d4d5bbc
Jon <jferretti6890@gmail.com>
2013-09-25 14:35:05 -0400

    Added alarm call backs, commandline args

--- pom.py
+++ pom.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
 from time import sleep
 from functools import partial
+from argparse import ArgumentParser
+from sys import exit
+
 
 class BaseTimer(object):
     """ Records current time and measures against time increments
@@ -8,8 +11,8 @@ class BaseTimer(object):
     """
 
     def set_start_time(self):
-        self.work_alarm = self.rest_alarm = datetime.now()
-        self.alarm_ring_time = [self.rest_alarm, self.work_alarm]
+        self.work_alarm_time = self.rest_alarm_time = datetime.now()
+        self.alarm_ring_time = [self.rest_alarm_time, self.work_alarm_time]
         self.alarm_deltas = [None, None]
 
     def set_time(self, is_work=False, hours=0, minutes=0, seconds=0):
@@ -35,26 +38,51 @@ class PomTimer(BaseTimer):
         self.set_rest_time = partial(self.set_time, is_work=False)
         self.alarm_setters = [self.set_rest_time, self.set_work_time]
 
+    def work_alarm(self):
+        print "Work over!"
+
+    def rest_alarm(self):
+        print "Break over!"
 
     def reset_alarm(self, next_alarm):
         self.alarm_setters[next_alarm](**self.alarm_deltas[next_alarm])
 
+    def start_timer(self):
+        self.alarm_callbacks = [self.rest_alarm, self.work_alarm]
+        alarm_counter = 1
+        while 1:
+            sleep(5)
+            if self.current_time() > self.alarm_ring_time[alarm_counter%2]:
+               self.alarm_callbacks[alarm_counter%2]()
+               alarm_counter += 1
+               self.reset_alarm(alarm_counter%2)
 
 
-def alarm():
-    print "time up!!!"
+def parse_input_time(input_time, output_time):
+    try:
+        input_time = [int(i) for i in input_time.split(":")]
+        return {"hours":input_time[0],
+                "minutes":input_time[1],
+                "seconds":input_time[2]}
+    except:
+        print "Input error"
+        print "Usage:"
+        print "pom.py --worktime=HH:MM:SS --breaktime=HH:MM:SS"
+        exit(1)
+
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--worktime", dest="work_time", type=str)
+    parser.add_argument("--breaktime", dest="rest_time", type=str)
+    args = parser.parse_args()
+
+    work_time = rest_time = datetime.now()
+    work_time = parse_input_time(args.work_time, work_time)
+    rest_time = parse_input_time(args.rest_time, rest_time)
 
     timer = PomTimer()
-    timer.set_work_time(minutes=30)
-    timer.set_rest_time(minutes=5)
-
-    counter = 1
-    while 1:
-        sleep(5)
-        if timer.current_time() > timer.alarm_ring_time[counter%2]:
-            alarm()
-            counter += 1
-            timer.reset_alarm(counter%2)
+    timer.set_work_time(**work_time)
+    timer.set_rest_time(**rest_time)
+    timer.start_timer()
