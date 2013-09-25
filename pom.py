dc6a1ad2b0b9f0f94677b1d2e4932fac99749fcd
Jon <jferretti6890@gmail.com>
2013-09-25 08:53:10 -0400

    finished time reseter

--- pom.py
+++ pom.py
@@ -9,20 +9,20 @@ class BaseTimer(object):
 
     def set_start_time(self):
         self.work_alarm = self.rest_alarm = datetime.now()
-        self.alarms = [self.rest_alarm, self.work_alarm]
-        self.alarm_times = [None, None]
+        self.alarm_ring_time = [self.rest_alarm, self.work_alarm]
+        self.alarm_deltas = [None, None]
 
     def set_time(self, is_work=False, hours=0, minutes=0, seconds=0):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
-        if not self.alarm_times[is_work]:
-            self.alarm_times[is_work] = {"hours":hours,
+        if not self.alarm_deltas[is_work]:
+            self.alarm_deltas[is_work] = {"hours":hours,
                                          "minutes":minutes,
                                          "seconds":seconds}
 
-        delta = self.alarm_times[is_work]
-        self.alarms[is_work] = datetime.now() + timedelta(**delta)
+        delta = self.alarm_deltas[is_work]
+        self.alarm_ring_time[is_work] = datetime.now() + timedelta(**delta)
 
     def current_time(self):
         return datetime.now()
@@ -33,6 +33,11 @@ class PomTimer(BaseTimer):
         self.set_start_time()
         self.set_work_time = partial(self.set_time, is_work=True)
         self.set_rest_time = partial(self.set_time, is_work=False)
+        self.alarm_setters = [self.set_rest_time, self.set_work_time]
+
+
+    def reset_alarm(self, next_alarm):
+        self.alarm_setters[next_alarm](**self.alarm_deltas[next_alarm])
 
 
 
@@ -43,12 +48,13 @@ def alarm():
 if __name__ == "__main__":
 
     timer = PomTimer()
-    timer.set_work_time(minutes=1)
-    timer.set_rest_time(minutes=2)
+    timer.set_work_time(minutes=30)
+    timer.set_rest_time(minutes=5)
 
     counter = 1
     while 1:
         sleep(5)
-        if timer.current_time() > timer.alarms[counter%2]:
+        if timer.current_time() > timer.alarm_ring_time[counter%2]:
             alarm()
             counter += 1
+            timer.reset_alarm(counter%2)
